a9b69947742a7041999c3554d5cbdec42df2ce01
33225507 <43640616+33225507@users.noreply.github.com>
2019-03-16 10:15:01 +0800

    Create 小克要裝書.cpp

---
+++ 小克要裝書.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std; 
+int main() {
+    stack <int> box;
+    vector<int> output;
+    int n, m;
+    cin >> n >> m;
+    int book = 1;
+    for (int i = 0; i < m; i++) {
+        char d;
+        cin >> d;
+        if (d == 'a') {
+           int t;
+           cin >> t;
+           for (int j = 0; j < t; j++) {
+               box.push(book);
+               book++;
+           }
+        }
+        else if (d == 'b') {
+                int t;
+                cin >> t;
+                for (int j = 0; j < t; j++) {
+                    output.push_back(box.top());
+                    box.pop();
+                }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << output[i];
+        if (i != n - 1) {
+           cout << " ";
+        }
+    }
+}
